eecacdd6caea5830124a75a0ee23113f2326db5e
Your Name <you@example.com>
2022-10-19 22:02:56 +0600

    work in slice and splice

---
+++ array-slice.js
@@ -0,0 +1,13 @@
+const numbers = [12, 23, 23, 43, 53, 53, 75, 78, 23, 45, 15, 62];
+// const arraySlice = numbers.slice(2, 8);
+// console.log(arraySlice);
+// console.log(numbers);
+
+// splice - remove data from an array 
+/* const spliceNumbers = numbers.splice(2, 8, 222, 3333, 5555, 77777);
+console.log(spliceNumbers);
+console.log(numbers); */
+const names = ['john', 'Deo', 'Wille', 'Thapa'];
+const nameSplice = names.splice(2, 3, 'Froud', 'Shy', 'sky');
+console.log(nameSplice);
+console.log(names);
